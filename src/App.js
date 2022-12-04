d87f3d6c27cc87214c766edbb7dc0867ffa02a06
FaIna <105994688+Faina4@users.noreply.github.com>
2022-12-04 13:51:16 +0200

    Added weather-app
    changed background color
    added border, paddings margins

--- src/App.js
+++ src/App.js
@@ -5,19 +5,10 @@ import Footer from './Footer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Weather-app">
+      <div className="weather-app-wrapper">
+      Current location
+      </div>
       </header>
       <footer>
        <Footer />
--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,19 @@
 import React from "react";
+import "./Footer.css"
 
 export default function Footer() {
-  return (<small>
-    <a
-      href="https://github.com/Faina4/WeatherApp-from-scratch"
+  return (
+  <small className="footer-info">
+  
+   <a
+      href="https://github.com/Faina4/react-weather-app-from-scratch-1"
       target="_blank"
       rel="noreferrer"
     >
-      Open-source code
-    </a>
+      Open-source code 
+    </a> {""}
     by FaIna
+   
   </small>)
   
 }
